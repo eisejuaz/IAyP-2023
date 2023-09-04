1ba60c9056f69deb4bb9998f1c238c05bc07aeb8
Franco Gargiulo <animalitof@gmail.com>
2023-09-04 18:59:15 -0300

    formateo

--- Laboratorio11-GargiuloFranco.c
+++ Laboratorio11-GargiuloFranco.c
@@ -21,40 +21,27 @@ int main () {
 
     // cargo el ultimo nodo, que queda apuntando a null en su next
     p = (TNodo * ) malloc (sizeof(TNodo));
-
     strcpy(p -> info, "va?");
-
     p -> next = NULL;
-
     p -> back = NULL;
 
     // creo otro y los enlazo
-    t = (TNodo * ) malloc (sizeof(TNodo));
-    
+    t = (TNodo * ) malloc (sizeof(TNodo));    
     p -> back = t;
-
     strcpy(t -> info, "te");
-
     t -> next = p;
 
     // creo otro y los enlazo
     s = (TNodo * ) malloc (sizeof(TNodo));
-
-    t -> back = t;
-    
+    t -> back = t;    
     strcpy(s -> info, "como");
-
     s -> next = t;
 
     // creo otro y los enlazo
     r = (TNodo * ) malloc (sizeof(TNodo));
-
     s -> back = s;
-
     strcpy(r -> info, "Hola");
-
     r -> next = s;
-
     r -> back = NULL;
 
     MostrarLista(r);
