bca4365d4bf8f38bd87d99bca9b5f7b49fcb765a
eisejuaz <animalitof@gmail.com>
2023-09-07 23:32:32 -0300

    corrijo estilos

--- Laboratorio11-GargiuloFranco.c
+++ Laboratorio11-GargiuloFranco.c
@@ -8,73 +8,72 @@
 #include <string.h>
 
 typedef struct nodo {
-    char info[10];
-    struct nodo * next;
-    struct nodo * back;
+  char info[10];
+  struct nodo *next;
+  struct nodo *back;
 } TNodo;
 
 TNodo *p, *q, *r, *s, *t;
 
-void MostrarLista(TNodo * list);
+void MostrarLista(TNodo *list);
 
-int main () {
+int main() {
+  // cargo el ultimo nodo, que queda apuntando a null en su next
+  p = (TNodo *)malloc(sizeof(TNodo));
+  strcpy(p->info, "va?");
+  p->next = NULL;
+  p->back = NULL;
 
-    // cargo el ultimo nodo, que queda apuntando a null en su next
-    p = (TNodo * ) malloc (sizeof(TNodo));
-    strcpy(p -> info, "va?");
-    p -> next = NULL;
-    p -> back = NULL;
+  // creo otro y los enlazo
+  t = (TNodo *)malloc(sizeof(TNodo));
+  p->back = t;
+  strcpy(t->info, "te");
+  t->next = p;
 
-    // creo otro y los enlazo
-    t = (TNodo * ) malloc (sizeof(TNodo));    
-    p -> back = t;
-    strcpy(t -> info, "te");
-    t -> next = p;
+  // creo otro y los enlazo
+  s = (TNodo *)malloc(sizeof(TNodo));
+  t->back = t;
+  strcpy(s->info, "como");
+  s->next = t;
 
-    // creo otro y los enlazo
-    s = (TNodo * ) malloc (sizeof(TNodo));
-    t -> back = t;    
-    strcpy(s -> info, "como");
-    s -> next = t;
+  // creo otro y los enlazo
+  r = (TNodo *)malloc(sizeof(TNodo));
+  s->back = s;
+  strcpy(r->info, "Hola,");
+  r->next = s;
+  r->back = NULL;
 
-    // creo otro y los enlazo
-    r = (TNodo * ) malloc (sizeof(TNodo));
-    s -> back = s;
-    strcpy(r -> info, "Hola,");
-    r -> next = s;
-    r -> back = NULL;
+  MostrarLista(r);
 
-    MostrarLista(r);
+  // creo a Tito y lo enlazo
+  q = (TNodo *)malloc(sizeof(TNodo));
+  strcpy(q->info, "Tito,");
+  q->next = r->next;
+  q->back = r;
+  r->next = q;
 
-    // creo a Tito y lo enlazo
-    q = (TNodo * ) malloc (sizeof(TNodo));
-    strcpy(q -> info, "Tito,");
-    q -> next = r -> next;
-    q -> back = r;
-    r -> next = q;
-    
-    MostrarLista(r);
+  MostrarLista(r);
 
-    // remplazo la ultima parte de la pregunta
-    s = p -> back;
-    strcpy(s -> info, "estas?");
-    s -> next = NULL;
-    s -> back = p -> back -> back;
-    p = s;
+  // remplazo la ultima parte de la pregunta
+  s = p->back;
+  strcpy(s->info, "estas?");
+  s->next = NULL;
+  s->back = p->back->back;
+  p = s;
 
-    MostrarLista(r);
+  MostrarLista(r);
 
-    return 0;
+  return 0;
 }
 
-void MostrarLista (TNodo * list) {
-    TNodo *aux;
-    aux = list;
+void MostrarLista(TNodo *list) {
+  TNodo *aux;
+  aux = list;
 
-    while(aux != NULL) {
-        printf("%s ",aux -> info);
-        aux = aux -> next;
-    };
-    
-    printf("\n");
+  while (aux != NULL) {
+    printf("%s ", aux->info);
+    aux = aux->next;
+  };
+
+  printf("\n");
 };
