2e539ad44b2b2cf70a2d932293d70a771c2178ed
unknown <animalitof@gmail.com>
2023-11-08 16:54:02 -0300

    subo avances de parcial

---
+++ Parcial4-GargiuloFranco.c
@@ -0,0 +1,55 @@
+// Materia: Introducción a la algorítmica y programación
+// Año: 2023
+// Comisión: 3
+// Alumno: Franco Gargiulo
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+// lexico
+#define MAX 100
+
+typedef struct {
+  int a[MAX];
+  int cant;
+} TData;
+
+TData datos;
+
+int i, pos, num;
+
+char msg[150];
+
+// perfiles
+void CrearDatos(TData *d);
+
+// main function
+int main() {
+
+    srand(time(NULL));
+
+    strcpy(msg, "Ingrese la cantidad de elementos a carga: ");
+
+    printf("%s", msg);
+
+    scanf("%d", &datos.cant);
+
+    printf("\ntengo lista la cant : %d", datos.cant);
+
+    CrearDatos(&datos);
+
+    return 0;
+}
+
+// implementaciones
+void CrearDatos(TData *d) {
+    int i;
+
+    for (i = 0; i < (*d).cant ; i++) {
+        d->a[i] = rand() % (100 - 1) + 1;
+        printf("\nNumero %d es: %d", i+1, d->a[i] );
+    };    
+};
+
